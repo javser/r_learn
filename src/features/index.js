4d54ec838aa6cec8515a195e90e5d5c696c85608
Serg <34738549+javser@users.noreply.github.com>
2022-03-31 17:12:58 +0300

    initial commit

---
+++ src/features/index.js
@@ -0,0 +1,13 @@
+/*
+ * search feature(взял вэлью инпута и куда-то поместил или кому-то отдал
+  ("сервис" получения данных по ключу из бэка, 
+  "сервис" фильтрации по ключу из стора или коллекции)
+   результат куда-то поместил или кому-то отдал (в стор или в энтити рендера данных))
+ * add bookmark
+ * send feedback
+ * show modal
+ * reducer + action
+ * ui feature + action feature
+ * local state + can get global state
+ * 
+ */
---
+++ src/processes/index.js
@@ -0,0 +1,6 @@
+/*
+ * fork sagas
+ * roles for actions
+ * example: create flag 'canPay' if authorized user & prod in basket
+ * ex: get metrics about actions on app
+ */
